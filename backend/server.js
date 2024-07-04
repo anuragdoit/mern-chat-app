4a24a0b1d224286ed6796b9b2f39474062a2b993
Gadde Anurag <gaddeanuragsreevastav@gmail.com>
2024-07-04 15:54:35 +0530

    4th part completed

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
 import connectToMongoDB from './db/connectToMongoDB.js'
+import { app, server } from './socket/socket.js'
 
 dotenv.config()
-const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
@@ -24,7 +24,7 @@ app.use('/api/users', userRoutes)
 //   res.send('Hello World!')
 // })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB()
   console.log(`Server Running on port ${PORT}`)
 })
